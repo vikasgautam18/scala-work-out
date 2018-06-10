42055aab810f517da541c573a639fab7d3d58f64
vikasgautam <vikas.gautam18@gmail.com>
2018-06-10 20:02:28 +0200

    recursion

---
+++ src/main/scala/gautamspeakscom/wordpress/com/recursion/SquareRoot.scala
@@ -0,0 +1,25 @@
+package gautamspeakscom.wordpress.com.recursion
+
+object SquareRoot{
+
+  def newtons_way(num: Double, guess: Double = 1) : Double = {
+    if(isGoodEnough(num, guess)) guess
+    else newtons_way(num, improve_guess(num, guess))
+
+  }
+  def abs(x: Double) = if(x > 0) x else -x
+
+  def isGoodEnough(d: Double, guess: Double) : Boolean = {
+    abs(guess * guess - d) < 0.001
+  }
+
+  def improve_guess(num: Double, guess: Double) : Double = {
+    (guess + num/guess)/2
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(newtons_way(2))
+
+    println(newtons_way(19))
+  }
+}

0ee84c3dca69dbc1af4c38b2d112cd3b2d4275de
vikasgautam <vikas.gautam18@gmail.com>
2018-10-13 23:30:32 +0200

    Day1 - more tests

--- src/test/scala/gautamspeakscom/wordpress/com/dailyCodingProblem/day1Test.scala
+++ src/test/scala/gautamspeakscom/wordpress/com/dailyCodingProblem/day1Test.scala
@@ -6,9 +6,9 @@ import org.scalatest.FunSuite
 class day1Test extends FunSuite {
 
   test("testInterleaf_with_List") {
-    val input = List(1, 2, 3, 4, 5)
-
-    interleaf_with_List(input) === List(1, 5, 2, 4, 3)
+    interleaf_with_List(List(1, 2, 3, 4, 5)) === List(1, 5, 2, 4, 3)
+    interleaf_with_List(List(1, 2, 3, 4)) === List(1, 5, 2, 4)
+    interleaf_with_List(List()) === List()
   }
 
 }
